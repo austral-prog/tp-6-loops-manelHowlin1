ef2333d71a4921328c5bd4894809c0f5b4a6df74
manelHowlin1 <mhowlin@mail.austral.edu.ar>
2026-04-14 12:49:31 -0300

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,3 @@
-# Replace the "ANSWER HERE" for your answer
-
 def index_of(target, lst):
     """
     Retorna el indice de la primera ocurrencia de target en lst.
@@ -7,8 +5,17 @@ def index_of(target, lst):
 
     Ejemplo: index_of("Black", ["Red", "Green", "Black"]) -> 2
     """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    indice = -1
+    for n in lst:
+        if target in lst:
+            if target != n:
+                indice = indice + 1
+            elif target == n:
+                indice = indice + 1
+                return indice
+        else:
+            return -1
+    return indice
 
 def index_of_by_index(target, lst, start):
     """
@@ -18,7 +25,15 @@ def index_of_by_index(target, lst, start):
 
     Ejemplo: index_of_by_index("Black", ["Red", "Black", "Green", "Black"], 2) -> 3
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    lista = lst[start:]
+
+    if target in lista:
+        return index_of(target,lista) + start
+    else:
+        return -1
+
+
+
 
 
 def index_of_empty(lst):
@@ -28,4 +43,13 @@ def index_of_empty(lst):
 
     Ejemplo: index_of_empty(["Red", "", "Green"]) -> 1
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    indice = -1
+    if "" not in lst:
+        return -1
+    for n in lst:
+        if n == "":
+            indice = indice + 1
+            return indice
+        else:
+            indice += 1
+    return indice
