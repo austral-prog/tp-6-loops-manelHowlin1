7a5a9acdedb9a769726579aa9f5fcbb026dedb47
manelHowlin1 <mhowlin@mail.austral.edu.ar>
2026-04-20 16:50:57 -0300

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -1,5 +1,3 @@
-# Replace the "ANSWER HERE" for your answer
-
 def flatten(matrix):
     """
     Dada una lista de listas (matriz), retorna una unica lista
@@ -7,7 +5,11 @@ def flatten(matrix):
 
     Ejemplo: flatten([[1, 2], [3, 4], [5, 6]]) -> [1, 2, 3, 4, 5, 6]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    lista = []
+    for i in matrix:
+        for j in i:
+            lista.append(j)
+    return lista
 
 
 def row_sums(matrix):
@@ -17,9 +19,13 @@ def row_sums(matrix):
 
     Ejemplo: row_sums([[1, 2, 3], [4, 5, 6]]) -> [6, 15]
     """
-    return "ANSWER HERE"  # Remove this line and implement
-
-
+    lista = []
+    for i in matrix:
+        suma = 0
+        for j in i:
+            suma += j
+        lista.append(suma)
+    return lista
 def col_sums(matrix):
     """
     Dada una matriz (lista de listas de numeros), retorna una lista
@@ -28,4 +34,10 @@ def col_sums(matrix):
 
     Ejemplo: col_sums([[1, 2, 3], [4, 5, 6]]) -> [5, 7, 9]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    lista = []
+    for i in range(len(matrix[0])):
+        suma = 0
+        for j in range(len(matrix)):
+            suma += matrix[j][i]
+        lista.append(suma)
+    return lista
